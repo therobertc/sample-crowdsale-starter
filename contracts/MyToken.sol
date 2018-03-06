7f2b8b5a7bb0f3389b6042e15e06cbacae58d3ae
Robert J Calderon <robjcalderon@Roberts-MacBook-Pro.local>
2018-03-06 15:17:46 -0800

    modified token name

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract MyToken is MintableToken {
-  string public constant name = "My Token";
-  string public constant symbol = "MYT";
+  string public constant name = "Jetstox";
+  string public constant symbol = "JSTX";
   uint8 public constant decimals = 18;
 }
